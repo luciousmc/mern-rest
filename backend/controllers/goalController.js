1e95de848043c538e77939e36965f477ee50b378
Marlon Clay <luciousmc@icloud.com>
2022-03-12 10:57:49 -0800

    chore: express async handler added to project

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,36 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Access Goals' });
-};
+});
 
 // @desc    Set Goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('No text found in request.');
   }
   res.status(201).json({ message: 'Created Goal' });
-};
+});
 
 // @desc    Update Goal
 // @route   GET /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Updated Goal ${req.params.id}` });
-};
+});
 
 // @desc    Delete Goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Deleted Goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
