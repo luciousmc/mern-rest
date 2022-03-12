f14b1bdc97c075e84c3d5bdad07567b2547ebef3
Marlon Clay <luciousmc@icloud.com>
2022-03-12 13:38:22 -0800

    feat: add bcryptjs to project

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,12 +4,14 @@
 const loginUser = async (req, res) => {
   res.status(200).json({ message: 'User Logged In' });
 };
+
 // @desc    Register a user
 // @route   POST /api/users
 // @access  Public
 const registerUser = async (req, res) => {
   res.status(201).json({ message: 'User Registered Succesffully' });
 };
+
 // @desc    Get user info
 // @route   GET /api/users/me
 // @access  Public
