20903a3fac35a245cefb0acc4005bb74f12e9894
Marlon Clay <luciousmc@icloud.com>
2022-03-12 12:27:32 -0800

    chore: change stack property of error to undefined
    Stack property of the custom error handler set to undefined to not appear in production.

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(status).json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
   });
 };
 
