839a740402352256837678a39f2253ba3376d186
Marlon Clay <luciousmc@icloud.com>
2022-03-12 13:05:16 -0800

    feat: add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
