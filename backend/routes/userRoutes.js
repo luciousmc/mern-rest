a5505321d9023185001ce262925044b932663a29
Marlon Clay <luciousmc@icloud.com>
2022-03-12 17:49:00 -0800

    feat: add protected route via jwt

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,11 @@ const {
   loginUser,
   getUserInfo,
 } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getUserInfo);
+router.get('/me', protect, getUserInfo);
 
 module.exports = router;
