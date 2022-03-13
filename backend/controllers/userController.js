c41dc50378ec5c0c09dd849168f851834190cfc9
Marlon Clay <luciousmc@icloud.com>
2022-03-12 17:09:10 -0800

    feat: create login user function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,20 @@ const User = require('../models/userModel');
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'User Logged In' });
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid Credentials');
+  }
 });
 
 // @desc    Register a user
