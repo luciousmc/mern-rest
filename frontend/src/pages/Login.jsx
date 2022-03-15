06bcdede733bdd5f6add37764f31adabe342be07
Marlon Clay <luciousmc@icloud.com>
2022-03-15 03:53:02 -0700

    feat: add register/login forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,51 @@
+import { useState } from 'react';
+import './Register.css';
+
 function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>Login</h1>
+        <p>Enter your info to login</p>
+      </section>
+
+      <section className='form register'>
+        <form>
+          <input
+            type='text'
+            className='form-control'
+            id='email'
+            name='email'
+            placeholder='Email Address'
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            className='form-control'
+            id='password'
+            name='password'
+            placeholder='Enter Password'
+            onChange={handleChange}
+          />
+
+          <input className='btn submit' type='submit' value='Submit' />
+        </form>
+      </section>
+    </>
+  );
 }
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,68 @@
+import { useState } from 'react';
+import './Register.css';
+
 function Register() {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>Register</h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className='form register'>
+        <form>
+          <input
+            type='text'
+            className='form-control'
+            id='name'
+            name='name'
+            placeholder='Enter Your Name'
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            className='form-control'
+            id='email'
+            name='email'
+            placeholder='Email Address'
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            className='form-control'
+            id='password'
+            name='password'
+            placeholder='Enter Password'
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            className='form-control'
+            id='password2'
+            name='password2'
+            placeholder='Confirm Password'
+            onChange={handleChange}
+          />
+          <input className='btn submit' type='submit' value='Submit' />
+        </form>
+      </section>
+    </>
+  );
 }
 export default Register;
