071103b2f96f0c141fad0ebe0ca40845d798e654
Marlon Clay <luciousmc@icloud.com>
2022-03-15 04:33:11 -0700

    feat: create global state for authUser
    authSlice created to keep track of the auth user

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Get user from local storage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+  user: user ? user : null,
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+  message: '',
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = '';
+    },
+  },
+});
+
+export const { reset } = authSlice.actions;
+
+export default authSlice.reducer;
