f5e6c3a949c8914b2cee8a4c0361c97ee5943bca
Marlon Clay <luciousmc@icloud.com>
2022-03-14 21:42:33 -0700

    feat: add header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/register' element={<Register />} />
